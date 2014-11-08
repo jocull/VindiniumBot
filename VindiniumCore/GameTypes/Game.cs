786c5867040bba7cf5080dca4c09b3dbb0413ce7
James O'Cull <James O'Cull@localhost>
2014-11-08 17:47:37 -0500

    Refactored base game types into game types

--- VindiniumCore/GameTypes/Game.cs
+++ VindiniumCore/GameTypes/Game.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VindiniumCore
+namespace VindiniumCore.GameTypes
 {
     public class Game
     {
--- VindiniumCore/GameTypes/Tile.cs
+++ VindiniumCore/GameTypes/Tile.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace VindiniumCore
+namespace VindiniumCore.GameTypes
 {
     public class Tile : Position
     {
