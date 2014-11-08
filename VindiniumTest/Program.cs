6565d4a296ece9993e57cc7fc5d8181d16d40ae4
James O'Cull <jocull@delmarsd.com>
2014-11-08 17:47:37 -0500

    Refactored base game types into game types

--- VindiniumTest/Program.cs
+++ VindiniumTest/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using VindiniumCore;
+using VindiniumCore.GameTypes;
 
 namespace VindiniumTest
 {
