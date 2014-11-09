d7d4375c7909a2fb11e80cf3b7bfbf2a2bef35da
James O'Cull <jocull@delmarsd.com>
2014-11-09 00:41:43 -0500

    Setup command line options

--- VindiniumBot/Program.cs
+++ VindiniumBot/Program.cs
@@ -10,7 +10,11 @@ namespace VindiniumBot
     {
         static void Main(string[] args)
         {
-
+            var options = new CommandLineOptions();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                // Values are available here
+            }
         }
     }
 }
