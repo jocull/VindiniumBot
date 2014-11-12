2c7488e5b5aaaf1035a8298238e1285b419d80f3
James O'Cull <James O'Cull@localhost>
2014-11-11 23:39:59 -0500

    Longer timeouts for game matches that don't happen right away

---
+++ VindiniumBot/BotWebClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VindiniumBot
+{
+    /// <summary>
+    /// A web request object with expanded timeout settings (30 minutes)
+    /// </summary>
+    internal class BotWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest w = base.GetWebRequest(address);
+            w.Timeout = (int)TimeSpan.FromMinutes(30).TotalMilliseconds;
+            return w;
+        }
+    }
+}
