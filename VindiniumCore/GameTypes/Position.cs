76b948155a6c1a4a231ac02081c9709b9320b020
James O'Cull <James O'Cull@localhost>
2014-11-13 21:21:07 -0500

    Proper Position overloading

--- VindiniumCore/GameTypes/Position.cs
+++ VindiniumCore/GameTypes/Position.cs
@@ -23,5 +23,41 @@ namespace VindiniumCore.GameTypes
         {
             return string.Format("X: {0}  Y: {1}", X, Y);
         }
+
+        public override bool Equals(object obj)
+        {
+            Position p = obj as Position;
+            if (p != null)
+            {
+                return p.X == this.X && p.Y == this.Y;
+            }
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            //See:
+            //http://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-an-overridden-system-object-gethashcode
+
+            return new { X = X, Y = Y }.GetHashCode();
+        }
+
+        public static bool operator ==(Position a, Position b)
+        {
+            if ((object)a == null && (object)b == null)
+            {
+                return true;
+            }
+            else if ((object)a != null && (object)b != null)
+            {
+                return a.X == b.X && a.Y == b.Y;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Position a, Position b)
+        {
+            return !(a == b);
+        }
     }
 }
